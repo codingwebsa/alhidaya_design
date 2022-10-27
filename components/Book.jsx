725437bf346f999a3e0cdcf0772f22480c525e1c
codingwebsa <mdsaifahamed530@gmail.com>
2022-10-27 18:13:12 +0600

    implemented category

--- components/Book.jsx
+++ components/Book.jsx
@@ -2,17 +2,16 @@ import Image from "next/image";
 
 const Book = ({ data }) => {
   return (
-    <div className="">
-      {data && (
-        <Image
-          src={data.imgUrl}
-          width={140}
-          height={170}
-          objectFit="cover"
-          className="rounded-lg drop-shadow-md"
-        />
-      )}
-    </div>
+    <article className="flex flex-col">
+      <Image
+        src={data.imgUrl}
+        width={140}
+        height={170}
+        objectFit="cover"
+        className="rounded-lg drop-shadow-md"
+      />
+      <p>{data.name}</p>
+    </article>
   );
 };
 
--- components/HomeCategory.jsx
+++ components/HomeCategory.jsx
@@ -5,19 +5,36 @@ import Category3 from "../assets/category3.webp";
 import Category4 from "../assets/category4.webp";
 
 const HomeCategory = () => {
+  const data = [
+    {
+      name: "সাহিত্য",
+      imgURL: Category1,
+    },
+    {
+      name: "ইসলামিক",
+      imgURL: Category2,
+    },
+    {
+      name: "ইতিহাস",
+      imgURL: Category3,
+    },
+    {
+      name: "নবী",
+      imgURL: Category4,
+    },
+  ];
   return (
     <div className="px-2 py-2">
       <h2 className="text-2xl font-bold pb-3">Categories</h2>
       <div className="HOMECATEGORY">
-        <Image
-          src={Category1}
-          className="relative rounded-xl"
-          quality={50}
-          data-category="বাংলা হাদিস"
-        />
-        <Image src={Category2} className="rounded-xl" quality={50} />
-        <Image src={Category3} className="rounded-xl" quality={50} />
-        <Image src={Category4} className="rounded-xl" quality={50} />
+        {data?.map((category, i) => (
+          <div className="relative flex rounded-xl overflow-hidden" key={i}>
+            <Image src={category.imgURL} className="rounded-xl" quality={50} />
+            <p className="absolute h-full w-full flex justify-center items-center text-2xl font-bold bg-[rgba(0,0,0,0.4)] text-soft">
+              {category.name}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
